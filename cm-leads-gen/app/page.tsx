65186b2bf2f6e554d75e4aee059fbdf9ba2e4f50
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2023-07-05 10:44:44 +0200

    Daisyui

--- cm-leads-gen/app/page.tsx
+++ cm-leads-gen/app/page.tsx
@@ -1,113 +1,105 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+        <html data-theme="dark">
+            <div className="overflow-hidden">
+              <div>
+                <h1 className="text-4xl font-bold tracking-tight leading-none md:text-5xl lg:text-6xl text-center mt-5">
+                  Vind med
+                </h1>
+                <img
+                  src="https://www.byggematerialer.dk/andel-energi/firma-files/Andel-Energi-Logo-stor.png"
+                  className="-mt-8 mx-auto"
+                />
+              </div>
+              <div className="w-full m-auto pr-5 pl-5 rounded-md shadow-md lg:max-w-6xl md:max-w-5xl">
+                <div className="hero bg-base-200">
+                  <div className="hero-content flex-col lg:flex-row">
+                    <img
+                      src="https://static.wixstatic.com/media/f758ab_8238ccd245614fb4a4f008c772f43598~mv2.png/v1/fill/w_560,h_438,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/SuperGavekortet_DK_Physical_Christmas_Front_Flat.png"
+                      className="max-w-full rounded-lg shadow-2xl lg:order-last lg:max-w-full lg:w-1/2 object-cover object-center"
+                      alt="MacBook"
+                    />
+                    <div className="lg:w-1/2">
+                      <h1 className="text-4xl font-bold">
+                        Vind et gavekort på 2.000 kr!
+                      </h1>
+                      <p className="pt-6 font-bold">
+                        Vær med i lodtrækningen om en fantastisk chance for at vinde
+                        et Super-Gavekort på 2.000 kr, der åbner dørene til en
+                        verden af muligheder. Med dette gavekort kan du forkæle dig
+                        selv og dine nærmeste med uforglemmelige oplevelser og
+                        shopping i en bred vifte af butikker og fornøjelser!
+                      </p>
+                      <p className="py-6 text-sm">
+                        Deltag i konkurrencen om et Super-Gavekort på 2.000 kr og
+                        bliv kontaktet via telefonopkald, e‑mail og sms/mms med gode
+                        tilbud på dine energiløsninger. Der trækkes lod om 1
+                        gavekort blandt alle deltagende. Du skal være over 18 år for
+                        at deltage i konkurrencen.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <form className="space-y-4">
+                  <div>
+                    <label className="label">
+                      <span className="text-base label-text">Navn</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Navn"
+                      className="w-full input input-bordered input-primary"
+                    />
+                  </div>
+                  <div>
+                    <label className="label">
+                      <span className="text-base label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="E-mail adresse"
+                      className="w-full input input-bordered input-primary"
+                    />
+                  </div>
+                  <div>
+                    <label className="label">
+                      <span className="text-base label-text">Mobil</span>
+                    </label>
+                    <input
+                      type="tel"
+                      placeholder="Telefonnummer"
+                      pattern="[0-9]+"
+                      className="w-full input input-bordered input-primary"
+                    />
+                  </div>
+                  <div>
+                    <label class="label cursor-pointer">
+                      <input type="checkbox" class="checkbox" />
+    
+                      <span class="label-text pl-8">
+                        Jeg giver samtykke til, at Andel Energi må kontakte mig via
+                        telefonopkald, e-mail og sms/mms med markedsføring om Andel
+                        Energi og deres produktsortiment. Samtykket må også bruges
+                        til at spørge mig, om jeg ønsker at udvide mit samtykke ift.
+                        markedsførings- og/eller databeskyttelsesreglerne. Samtykket
+                        kan altid tilbagekaldes, se hvordan{" "}
+                        <span className="underline">her.</span> Du kan læse i vores
+                        persondatapolitik, hvordan vi behandler oplysninger om dig.
+                      </span>
+                    </label>
+                    <button class="btn btn-block btn-primary">
+                      DELTAG I KONKURRENCEN
+                    </button>
+                    <p className="text-center mt-6">
+                      Konkurrencen løber fra den 16. Jan 2023 til og med den 14.
+                      Februar 2023.
+                    </p>
+                  </div>
+                  <div class="form-control"></div>
+                </form>
+              </div>
+            </div>
+        </html>
+      );
+    }
+    
